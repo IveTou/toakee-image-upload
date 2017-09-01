51aeb546efca685a379289217a87018dc4444277
ive <ivetourinho@gmail.com>
2017-09-01 03:14:17 -0300

    With preview list

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Button, Grid, Header, Icon, Image, Item, List } from 'semantic-ui-react';
+import { Container, Button, Grid, Header, Icon, Image, Item, List, Segment } from 'semantic-ui-react';
 import autoBind from 'react-autobind';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
@@ -13,9 +13,9 @@ const { CLOUDINARY_API_URI, UPLOAD_FLYER_PRESET } = config;
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { files: '', filesUrl: '' };
+    this.state = { files: {}, filesUrl: {} };
     autoBind(this);
-    console.log(process.env.BROWSER);
+    console.log(process.env);
   }
 
   onImageDrop(files) {
@@ -37,6 +37,40 @@ class App extends React.Component {
       });
   }
 
+  renderSelectedImages() {
+    const { files } = this.state;
+    if (!!!files.length) {
+      return (
+        <Item>
+          <Item.Image
+            name="name"
+            size="tiny"
+            src="https://react.semantic-ui.com/assets/images/wireframe/image.png"
+          />
+          <Item.Content>
+            <label>There is no images yet.</label>
+          </Item.Content>
+        </Item>
+      );
+    } else {
+      return files.map((file) =>
+        <Item>
+          <Item.Image name="name" size="tiny" src={file.preview} />
+          <Item.Content>
+            <label>{file.name}</label>
+            <Button>delete</Button>
+          </Item.Content>
+        </Item>
+      );
+    }
+  }
+
+  renderImageUrlList() {
+    return (
+      <List.Item>{}</List.Item>
+    );
+  }
+
   render() {
     const { files, filesUrl } = this.state;
     const hasFile = !!files.length;
@@ -47,33 +81,25 @@ class App extends React.Component {
           <Header as="h1">Image Upload Page</Header>
         </Grid.Row>
         <Grid.Row>
-        <Grid.Column width={4}>
+        <Grid.Column width={2}>
           <Header as="h3">Drop Zone</Header>
-          <Dropzone
-            className="DropZone"
-            multiple={false}
-            accept="image/*"
-            onDrop={this.onImageDrop}
-          >
-            <div>
-              <Icon name='upload' size='huge' color="blue" />
-              <p>Drop files here or click to upload</p>
-            </div>
-          </Dropzone>
+          <Segment>
+            <Dropzone
+              className="DropZone"
+              multiple={true}
+              accept="image/*"
+              onDrop={this.onImageDrop}
+            >
+              <div>
+                <Icon name='upload' size='huge' color="blue" />
+                <p>Drop files here or click to upload</p>
+              </div>
+            </Dropzone>
+          </Segment>
         </Grid.Column>
-        <Grid.Column width={4}>
+        <Grid.Column width={6}>
           <Header as="h3">Selected Images</Header>
-          <Item.Group>
-
-            <Item>
-              <Item.Image name="name" size="tiny" src="https://react.semantic-ui.com/assets/images/wireframe/image.png" />
-              <Item.Content>
-                <p>Description</p>
-                <Button>delete</Button>
-              </Item.Content>
-            </Item>
-
-          </Item.Group>
+          <Item.Group>{this.renderSelectedImages()}</Item.Group>
           <Button
             inverted
             color="orange"
@@ -85,11 +111,9 @@ class App extends React.Component {
         <Grid.Column width={8}>
           <Header as="h3" color="orange">Uploaded Images</Header>
           <List>
-
             <List.Item>Apples</List.Item>
             <List.Item>Pears</List.Item>
             <List.Item>Oranges</List.Item>
-
           </List>
           <Button
             inverted

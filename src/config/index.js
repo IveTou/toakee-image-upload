e51246fed8dc01c78bc95ea60fa533e00f1cac62
ive <ivetourinho@gmail.com>
2017-09-11 10:51:41 -0300

    Change preset

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 //const devMode = process.env.NODE_ENV !== 'production';
 
 export default {
-  UPLOAD_FLYER_PRESET: 'event-flyer-ex',
+  UPLOAD_FLYER_PRESET: 'event-flyer',
   CLOUDINARY_API_URI: 'https://api.cloudinary.com/v1_1/toakeeassets/image',
 };

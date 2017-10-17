a1b4e4c50973b0edcea362b848ae11016080b163
ive <ivetourinho@gmail.com>
2017-10-17 03:43:08 -0300

    New Config

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 //const devMode = process.env.NODE_ENV !== 'production';
 
 export default {
-  UPLOAD_FLYER_PRESET: 'event-flyer',
+  UPLOAD_FLYER_PRESET: 'event-flyer-new',
+  UPLOAD_PHOTO_PRESET: 'event-photo-new',
   CLOUDINARY_API_URI: 'https://api.cloudinary.com/v1_1/toakeeassets/image',
 };
